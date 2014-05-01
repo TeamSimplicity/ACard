21f3711e2c855a2a74237cd8e7ba15274aeeb141
tratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2014-05-01 12:05:01 -0700

    WORKING!

--- www/js/index.js
+++ www/js/index.js
@@ -1,82 +1,67 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+$( document ).ready(function() {
+            
 
-var app = {
-    initialize: function() {
-        this.bind();
-    },
-    bind: function() {
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-        // note that this is an event handler so the scope is that of the event
-        // so we need to call app.report(), and not this.report()
-        app.report('deviceready');
-    },
-    report: function(id) { 
-        console.log("report:" + id);
-        // hide the .pending <p> and show the .complete <p>
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
-    }
-};
+      $('#splashpage').show();
+      $('#signupform').hide();
+      $('#page1').hide();
+      $('#page2').hide();
 
-$(document).on('deviceready', function(){
-	
-	function loadBugs() {
-		var bugs = $('#bugs ul').empty();
-		
-		$.ajax({
-			type: 'GET',
-			url: 'http://localhost/bapp/bugs.php?&jsoncallback=?',
-			dataType: 'JSONp',
-			timeout: 5000,
-			success: function(data) {
-				$.each(data, function(i,item){
-					bugs.append('<li>'+item.title)
-				});
-			},
-			error: function(data) {
-				bugs.append('<li>There was an error loading the bugs');
-			}
-		});
-	}
-	
-	
-	
-	//change .tap to .click for browser testing
-	$('.button').tap(function(e){
-		var nextPage = $(e.target.hash);
-		var currentPage = $('.page.current');
+      $('#signup').click(function(){
+        
+        $('#splashpage').fadeOut('slow');
+        $('#signupform').fadeIn('');
+        $('#page1').fadeIn('');
+        $('#page2').fadeIn('');
 
-		if (nextPage.attr('id') != currentPage.attr('id')) {
-			nextPage.addClass('current');
-			currentPage.removeClass('current');
-		}
+      });
 
-		return false;
-	});
-	
-	loadBugs();
-	
+
+
+        
+
+
+
+        //Submit button on form
+        $('#signup-form form').submit(function(){
+        alert("invoked");
+        var loading = $(this).find('input[type="submit"]');
+        loading.addClass('loading');
+        
+        var postData = $(this).serialize();
+
+        $.ajax({
+            type: 'POST',
+            data: postData,
+            url: 'http://tratnayake.me/Assign-Barcode.php',
+            success: function(data){
+             alert(data);
+             
+              var resultarray = data.split(".");
+              alert("after split");
+
+              var barcodenum = resultarray[0].replace(/\D/g,'');
+              alert(barcodenum);
+              //var res = data.replace(/\D/g,'')
+              var Name = resultarray[1];
+              alert(Name);
+
+              alert("Before display");
+
+                
+                $("#bcTarget").barcode(barcodenum, "codabar");
+                $("#User_Name").val(Name);   
+            alert ("after display");
+
+                
+                console.log('Form Sent!');
+            },
+            error: function(){
+                loading.removeClass('loading');
+                console.log('There was an error');
+            }
+        });
+
+        return false;
+    });
+
 });
-
-
-
