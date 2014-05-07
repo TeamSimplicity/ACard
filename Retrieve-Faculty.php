534450bfe59499596a529e6dadfd9129e2db93b2
ak1230 <anthonee1230@gmail.com>
2014-05-07 09:17:15 -0700

    select id from faculty

--- Retrieve-Faculty.php
+++ Retrieve-Faculty.php
@@ -8,7 +8,7 @@
 
 	mysql_select_db($database, $con);
 
-	$sql = "SELECT Faculty_Name FROM Faculty_Info";
+	$sql = "SELECT Faculty_ID, Faculty_Name FROM Faculty_Info";
 	//$sqlold = "SELECT id, title FROM bugs WHERE open = TRUE ORDER BY priority, date_time_added";
 	$result = mysql_query($sql) or die ("Query error: " . mysql_error());
 
